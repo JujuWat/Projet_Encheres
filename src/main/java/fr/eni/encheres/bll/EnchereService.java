6361fc8f8e2dd9bbf826bec27cfa280c5e2f1689
Amelie CAILLET <amelie.caillet2024@campus-eni.fr>
2024-12-16 17:02:49 +0100

    ajout methode BLL

--- src/main/java/fr/eni/encheres/bll/EnchereService.java
+++ src/main/java/fr/eni/encheres/bll/EnchereService.java
@@ -1,10 +1,14 @@
 package fr.eni.encheres.bll;
 
-import jakarta.validation.constraints.AssertFalse.List;
+import java.util.List;
+import fr.eni.encheres.bo.ArticleVendu;
+import fr.eni.encheres.bo.Categorie;
 
 public interface EnchereService {
 	
-	//List<ArticleVendu> consulterArticles ();
+	List<ArticleVendu> consulterArticles ();
+	List<Categorie> consulterCategories();
 	
-
+	ArticleVendu consulterParEtat (int etatVente);
+	
 }
--- src/main/java/fr/eni/encheres/controller/EnchereController.java
+++ src/main/java/fr/eni/encheres/controller/EnchereController.java
@@ -2,9 +2,15 @@ package fr.eni.encheres.controller;
 
 import org.springframework.stereotype.Controller;
 
+import fr.eni.encheres.bll.EnchereService;
+
 @Controller
 public class EnchereController {
-	//private EnchereService enchereService
+	private EnchereService enchereService;
+
+	public EnchereController(EnchereService enchereService) {
+		this.enchereService = enchereService;
+	}
 	
 	
 	
