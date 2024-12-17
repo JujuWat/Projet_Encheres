c01eba11eeaceb66cc32cde6ecddc3686b23d2f9
AmélieC <a.cailletrichard@gmail.com>
2024-12-17 09:26:36 +0100

    reparation

--- src/main/java/fr/eni/encheres/controller/EnchereController.java
+++ src/main/java/fr/eni/encheres/controller/EnchereController.java
@@ -3,8 +3,8 @@ package fr.eni.encheres.controller;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
 
 import fr.eni.encheres.bll.EnchereService;
 
@@ -17,16 +17,6 @@ public class EnchereController {
 		this.enchereService = enchereService;
 	}
 	
-	
-
-	
-	// Association : 
-	/* private EnchereService enchereService;
-	// Consructeur pour instancier l'association : 
-	public EnchereController(EnchereService enchereService) {
-		this.enchereService = enchereService;
-	} */
-
 		
 	@GetMapping("/")
 	public String afficherAccueil() {
