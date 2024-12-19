0072122c1c1e1d05c407a765358df8e5f2d958a4
JujuWat <julian.denoue@gmail.com>
2024-12-19 17:11:42 +0100

    Page Vendre un Objet

--- src/main/java/fr/eni/encheres/dal/RetraitsDAO.java
+++ src/main/java/fr/eni/encheres/dal/RetraitsDAO.java
@@ -1,5 +1,9 @@
 package fr.eni.encheres.dal;
 
+import fr.eni.encheres.bo.Retrait;
+
 public interface RetraitsDAO {
+	
+	void ajouterRetrait(Retrait retrait, int noArticle);
 
 }
--- src/main/java/fr/eni/encheres/dal/RetraitsDAOImpl.java
+++ src/main/java/fr/eni/encheres/dal/RetraitsDAOImpl.java
@@ -1,8 +1,37 @@
 package fr.eni.encheres.dal;
 
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+import fr.eni.encheres.bo.Retrait;
 
 @Repository
 public class RetraitsDAOImpl implements RetraitsDAO{
+
+	private static final String CREATE_RETRAIT = "INSERT INTO RETRAITS (no_article, rue, code_postal, ville) VALUES (:no_article, :rue, :code_postal, :ville)";
+	
+	private NamedParameterJdbcTemplate jdbcTemplate;
+	
+		
+	public RetraitsDAOImpl(NamedParameterJdbcTemplate jdbcTemplate) {
+		super();
+		this.jdbcTemplate = jdbcTemplate;
+	}
+
+
+	@Override
+	public void ajouterRetrait(Retrait retrait, int noArticle) {
+		 MapSqlParameterSource map = new MapSqlParameterSource();
+		    map.addValue("no_article", noArticle);  // Utilisation de 'map' ici
+		    map.addValue("rue", retrait.getRue());
+		    map.addValue("code_postal", retrait.getCode_postal());
+		    map.addValue("ville", retrait.getVille());
+
+		    jdbcTemplate.update(CREATE_RETRAIT, map);
+		
+	}
+	
+	
 
 }
